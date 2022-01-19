be038ae8d03da1456def244a8f571d1edbe95531
wpletcher <wpletcher@companieshouse.gov.uk>
2022-01-19 14:20:49 +0000

    add psc_pursuit_trigger

---
+++ weblogic-batch/psc-pursuit-trigger/psc-pursuit-trigger.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+cd /apps/oracle/psc-pursuit-trigger
+
+# load variables created from setCron script
+source /apps/oracle/env.variables
+# create properties file and substitutes values
+envsubst < psc-pursuit-trigger.properties.template > psc-pursuit-trigger.properties
+
+CLASSPATH=$CLASSPATH:.:/apps/oracle/libs/commons-logging.jar:/apps/oracle/libs/ojdbc8.jar:/apps/oracle/libs/log4j.jar:/apps/oracle/psc-pursuit-trigger/psc-pursuit-trigger.jar
+
+# set up logging
+LOGS_DIR=../logs/psc-pursuit-trigger
+mkdir -p ${LOGS_DIR}
+LOG_FILE="${LOGS_DIR}/${HOSTNAME}-psc-pursuit-trigger-$(date +'%Y-%m-%d_%H-%M-%S').log"
+
+exec >> ${LOG_FILE} 2>&1
+
+echo  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+echo `date` Starting psc-pursuit-trigger
+
+/usr/java/jdk-8/bin/java -Din=psc-pursuit-trigger -Dlog4j.configuration=log4j.xml -jar psc-pursuit-trigger.jar psc-pursuit-trigger.properties
+if [ $? -gt 0 ]; then
+        echo "Non-zero exit code for psc-pursuit-trigger java execution"
+        exit 1
+fi
+
+echo `date` Ending psc-pursuit-trigger
+echo  ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
