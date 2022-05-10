70aa3e637680cbbaa101a5ac2e1a1c09dfefad74
Jake Metherell <jake@fluffylogic.com>
2022-05-10 21:43:52 +0100

    Correct typo in comments

--- weblogic-batch/admin/jms/move-jms.sh
+++ weblogic-batch/admin/jms/move-jms.sh
@@ -12,7 +12,7 @@
 #  JMS SERVER URL - the t3 or t3s URL for the WebLogic server, e.g. t3s://1.2.3.4:1234
 #  USERNAME - e.g. weblogic
 #  PASSWORD - e.g. password
-#  NUMBER OF MESSAGES - the number of mesassages to move, e.g. 100
+#  NUMBER OF MESSAGES - the number of messages to move, e.g. 100
 #
 #  This script is intended to be called from another wrapper script and its main
 #  job is to set the CLASSPATH and any Java commandline parameters.
@@ -23,4 +23,3 @@ LIBS=/apps/oracle/libs
 CLASSPATH=${LIBS}/jmstool.jar:${LIBS}/log4j.jar:${LIBS}/jms-api.jar:${LIBS}/wlfullclient.jar:${LIBS}/jdom.jar
 
 /usr/java/jdk-8/bin/java -cp ${CLASSPATH} -Dweblogic.security.SSL.ignoreHostnameVerification=true -Dweblogic.MaxMessageSize=100000000 chaps.jms.MoveJMSMessages $*
-
