2fb93f774a7962783816a61fe6939deb1a7b4977
Venky <vnara@companieshouse.gov.uk>
2023-10-20 14:20:26 +0100

    CM-1569 Update comment

--- weblogic-batch/admin/jms/list-all-jms-image-error-queue.sh
+++ weblogic-batch/admin/jms/list-all-jms-image-error-queue.sh
@@ -2,19 +2,13 @@
 
 # =============================================================================
 #
-#  This script is used to list stuck EFiling JMS messages in the EfilingErrorQueue   
+#  This script is used to list stuck ImageApiErrorQueue JMS messages in the ImageApiMessageProducerErrorQueue   
 #  on all JMS servers.
 #
 #  It will display messages on each JMS server and show the object id 
-#  (which is useful in the Weblogic console), if the response is ACCEPTED or REJECTED,
-#  the barcode and either INVESTIGATE or OK_TO_DELETE. E.g:
-#  2023-07-06T15:41:51,709 [list-all-jms-ef-responses.sh] INFO  851960.1688648406753.0 REJECTED	 XBZ74XOB INVESTIGATE
+#  (which is useful in the Weblogic console),  E.g: OBJECT_ID JMSSERVER_URL PAYLOAD_NAME PAYLOAD_JSON
+#  2023-07-06T15:41:51,709 [list-all-jms-ef-responses.sh] INFO  745471.1697731224545.0  t3s://chips-ef-batch0.live.heritage.aws.internal:21031  TiffStreamPayload  {   "document_type": "RP05",   "category": "miscellaneous",   "barcode": "RCD2E3DK" ...}
 #
-#  If the barcode is not found on the ewf admin website or there is already a response 
-#  that differs from the JMS message then INVESTIGATE will be shown and it is not safe to delete the message.
-#  If the barcode is found and the response on the admin website is the same, then OK_TO_DELETE will be shown.
-# 
-#  No parameters are required, but there are a number of environment variables that need to be present:
 #
 #  One or more environment variables that list the connection details for the JMS Servers on which to 
 #  perform the list operation. These variables must be called JMS_SERVER_URL_#
